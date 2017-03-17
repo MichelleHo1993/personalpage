61cb0e1e3fd6cb693e45f9a36bab70dab484a76c
heyi <he.yi@etao.cn>
2017-03-17 16:37:35 +0800

    Pro: 添加首页滚动翻页

--- js/main.js
+++ js/main.js
@@ -65,16 +65,23 @@ function getwheel(){
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
         // console.log(delta > 0 ? "向上滚动" : "向下滚动");
         if(delta<0){
-            console.log(oSection2.style.top)
-            oSection2.style.top = 0;
+            console.log('<0',delta)
+
+            console.log('下')
+            scroller('section2', 500)
+        }
+        if(delta>0){
+            console.log('>0',delta)
+            scroller('section1', 500)
         }
     }
     if(window.navigator.userAgent.indexOf('Firefox')!=-1){
         oSection1.addEventListener("DOMMouseScroll", mousewheelHandler, false);
+        oSection2.addEventListener("DOMMouseScroll", mousewheelHandler, false);
         }else{
             oSection1.addEventListener("mousewheel", mousewheelHandler, false);
-            oSection2.style.top = 0;
-            console.log('下')
+            oSection2.addEventListener("mousewheel", mousewheelHandler, false);
+
     }
 }
 getwheel();
@@ -103,7 +110,7 @@ for(var i=0;i<aLi.length;i++){
     window.onscroll=function(){
 
         var scrollT = document.documentElement.scrollTop||document.body.scrollTop;
-    console.log(scrollT)
+    // console.log(scrollT)
 
         if(scrollT>greetingsTop+110){
             greetingsDiv.style.display = 'none';
@@ -117,3 +124,78 @@ for(var i=0;i<aLi.length;i++){
             place_holder.style.display = 'none';
         }
     }
+
+    //锚点js平滑滚动
+    function intval(v)
+{
+    v = parseInt(v);
+    return isNaN(v) ? 0 : v;
+}
+ 
+// 获取元素信息
+function getPos(e)
+{
+    var l = 0;
+    var t  = 0;
+    var w = intval(e.style.width);
+    var h = intval(e.style.height);
+    var wb = e.offsetWidth;
+    var hb = e.offsetHeight;
+    while (e.offsetParent){
+        l += e.offsetLeft + (e.currentStyle?intval(e.currentStyle.borderLeftWidth):0);
+        t += e.offsetTop  + (e.currentStyle?intval(e.currentStyle.borderTopWidth):0);
+        e = e.offsetParent;
+    }
+    l += e.offsetLeft + (e.currentStyle?intval(e.currentStyle.borderLeftWidth):0);
+    t  += e.offsetTop  + (e.currentStyle?intval(e.currentStyle.borderTopWidth):0);
+    return {x:l, y:t, w:w, h:h, wb:wb, hb:hb};
+}
+ 
+// 获取滚动条信息
+function getScroll() 
+{
+    var t, l, w, h;
+    
+    if (document.documentElement && document.documentElement.scrollTop) {
+        t = document.documentElement.scrollTop;
+        l = document.documentElement.scrollLeft;
+        w = document.documentElement.scrollWidth;
+        h = document.documentElement.scrollHeight;
+    } else if (document.body) {
+        t = document.body.scrollTop;
+        l = document.body.scrollLeft;
+        w = document.body.scrollWidth;
+        h = document.body.scrollHeight;
+    }
+    return { t: t, l: l, w: w, h: h };
+}
+ 
+// 锚点(Anchor)间平滑跳转
+function scroller(el, duration)
+{
+    if(typeof el != 'object') { el = document.getElementById(el); }
+ 
+    if(!el) return;
+ 
+    var z = this;
+    z.el = el;
+    z.p = getPos(el);
+    z.s = getScroll();
+    z.clear = function(){window.clearInterval(z.timer);z.timer=null};
+    z.t=(new Date).getTime();
+ 
+    z.step = function(){
+        var t = (new Date).getTime();
+        var p = (t - z.t) / duration;
+        if (t >= duration + z.t) {
+            z.clear();
+            window.setTimeout(function(){z.scroll(z.p.y, z.p.x)},13);
+        } else {
+            st = ((-Math.cos(p*Math.PI)/2) + 0.5) * (z.p.y-z.s.t) + z.s.t;
+            sl = ((-Math.cos(p*Math.PI)/2) + 0.5) * (z.p.x-z.s.l) + z.s.l;
+            z.scroll(st, sl);
+        }
+    };
+    z.scroll = function (t, l){window.scrollTo(l, t)};
+    z.timer = window.setInterval(function(){z.step();},13);
+}
